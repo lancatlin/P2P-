cd66f7cb7af2e4e7101a603ce3555c19424d82bb
lancatlin <lancat1499@gmail.com>
2017-11-15 21:33:23 +0800

    改變程式寫法
    將程式碼包進class，使用物件導向，刪除data.py

--- tkmain.py
+++ tkmain.py
@@ -1,68 +1,55 @@
 import socket,_thread,time,sys
 
-def main(ip,d):
-    def server(ADDR,ip):
-        nonlocal mode,data
-        s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-        s.bind(ADDR)
-        print(ADDR)
-        addr = ip,d.port
-        _thread.start_new_thread(test,(s ,addr,ADDR))
-        data , addr = s.recvfrom(1024)
-        mode = False
-        _print('已連接')
-        print(data)
-        s.sendto('ok'.encode('UTF-8'),addr)
-        s.sendto(data,(addr[0],d.port))
-        _thread.start_new_thread(user,(s ,addr))
+class net:
+    def __init__(self,ip,window):
+        self.window = window
+        self.port = int(self.window.portentry.get())
+        HOST, PORT = socket.gethostname(), self.port
+        HOST = socket.gethostbyname(HOST)
+        self.addr = (HOST, self.port)
+        self.mode = True
+        self.data = (window.nameentry.get()+',' + str(self.addr[0]) + ',' + str(self.addr[1])).encode()
+        time.sleep(0.5)
+        self.target = (ip,self.port)
+        _thread.start_new_thread(self.server,())
+    def server(self):
+        self.s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+        self.s.bind(self.addr)
+        print(self.addr)
+        #addr = ip,self.window.portentry.get()
+        _thread.start_new_thread(self.test,())
+        self.data , self.target = self.s.recvfrom(1024)
+        self.mode = False
+        self._print('已連接')
+        print(self.data)
+        self.s.sendto(self.data.encode('UTF-8'),self.target)
         while True:
-            data,addr = s.recvfrom(1024)
+            data,addr = self.s.recvfrom(1024)
             data = data.decode('UTF-8')
-            d.totext = data
+            self.window.add_new(data)
             time.sleep(0.5)
-        s.close
-    def user(s,addr):
-        global input
-        while True:
-            while input == '':
-                time.sleep(0.01)
-            data=input
-            now = time.strftime("[%H-%M]")
-            data=now+data
-            d.totext = '<You>'+data
-            if data == 'end':
-                break
-            data = '<'+d.name+'>'+data
-            data=data.encode('UTF-8')
-            s.sendto(data,addr)
-            input = ''
-        s.close
-        sys.exit()
-    def test(s,addr,ADDR):
-        nonlocal mode
-        nonlocal data
-        if mode:
-            print('準備連接'+str(addr))
+    def user(self,string):
+        now = time.strftime("[%H-%M]")
+        data=now+string
+        self.window.add_new('<You>'+data)
+        if self.data == 'end':
+            sys.exit()
+        self.data = '<'+self.window.nameentry.get()+'>'+data
+        self.data=self.data.encode('UTF-8')
+        self.s.sendto(data,self.target)
+    def test(self):
+        if self.mode:
+            print('準備連接'+str(self.target))
             x = 0
-            while mode and x <= 1000:
-                s.sendto(data,addr)
+            while self.mode and x <= 1000:
+                self.s.sendto(self.data,self.target)
                 time.sleep(0.2)
                 x+=1
-            if mode:
-                _print('連線失敗')
+            if self.mode:
+                self._print('連線失敗')
                 sys.exit()
             else:
-                _print('已連接')
-    def _print(s):
+                self._print('已連接')
+    def _print(self,s):
         string = str(s)
-        d.toprint = string
-    HOST,PORT = socket.gethostname(),d.port
-    HOST = socket.gethostbyname(HOST)
-    ADDR = (HOST,PORT)
-    mode = True
-    data = ('test,'+'lancat,'+str(ADDR[0])+','+str(ADDR[1])).encode()
-    time.sleep(0.5)
-    server(ADDR,ip)
-input=''
-if __name__ == '__main__':
-    main(input('輸入ip:'))
+        self.window._print_(string)
--- window.py
+++ window.py
@@ -1,99 +1,70 @@
 from tkinter import *
-import threading,tkmain,time,data
+import threading,tkmain,time
 
-d = data.data()
-def setIp():
-    global d
-    ip = str(ipentry.get())
-    _print_('準備連接:'+ip)
-    start = threading.Thread(target = tkmain.main,args=(ip,d))
-    start.setDaemon(True)
-    start.start()
-def enter():
-    tkmain.input = inputentry.get()
-    inputentry.delete(0,END)
-def _print_(s):
-    global info
-    print(s)
-    info.set(s)
-def port():
-    global d
-    while True:
-        if d.port != portentry.get():
-            try:
-                d.port = int(portentry.get())
-            except:
-                pass
-            portentry.delete(0,END)
-            portentry.insert(0,d.port)
-            time.sleep(2)
-        time.sleep(1)
-def waitprint():
-    global info
-    global d
-    while True:
-        if d.toprint != '':
-            _print_(d.toprint)
-            d.toprint = ''
-        if d.totext != '' :
-            talktext.insert(END,'\n'+d.totext)
-            d.totext = ''
-            talktext.see(END)
-            print(d.totext)
-        if d.name != nameentry.get():
-            d.name = nameentry.get()
-        time.sleep(0.05)
-window = Tk()
-window.title('P2P Talking')
-window.geometry('350x650')
+class window(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('P2P Talking')
+        self.geometry('350x650')
+        rows = 1
 
-rows=1
+        f1 = Frame(self)
+        f1.grid(row=rows, columnspan=5, sticky=W, pady=8, padx=5)
+        Label(f1, text='Ip:', width=5).grid(row=rows, sticky=W)
+        self.ipentry = Entry(f1, width=10)
+        self.ipentry.grid(row=rows, column=1)
+        Label(f1, text='port:', width=5).grid(row=rows, column=2)
+        self.portentry = Entry(f1, width=5)
+        self.portentry.grid(row=rows, column=3)
+        rows += 1
 
-f1 = Frame(window)
-f1.grid(row=rows,columnspan=5,sticky=W,pady=8,padx=5)
-Label(f1,text='Ip:',width=5).grid(row=rows,sticky=W)
-ipentry = Entry(f1,width=10)
-ipentry.grid(row=rows,column=1)
-Label(f1,text='port:',width=5).grid(row=rows,column=2)
-portentry = Entry(f1,width=5)
-portentry.grid(row=rows,column=3)
-rows+=1
+        f2 = Frame(self)
+        f2.grid(row=rows, columnspan=5, sticky=W, pady=8, padx=5)
+        Label(f2, text='使用者名稱:', width=10).grid(row=rows, sticky=W, columnspan=2)
+        self.nameentry = Entry(f2, width=10)
+        self.nameentry.grid(row=rows, column=2)
+        Button(f2, text='連接', width=5, command=lambda: self.setIp()).grid(row=rows, column=3)
+        rows += 1
 
-f2 = Frame(window)
-f2.grid(row=rows,columnspan=5,sticky=W,pady=8,padx=5)
-Label(f2,text='使用者名稱:',width=10).grid(row=rows,sticky=W,columnspan=2)
-nameentry = Entry(f2,width=10)
-nameentry.grid(row=rows,column=2)
-Button(f2,text='連接',width=5,command=lambda:setIp()).grid(row=rows,column=3)
-rows+=1
+        Label(self, text='聊天內容:', width=8).grid(row=rows, columnspan=2, sticky=W)
+        rows += 1
 
-Label(window,text='聊天內容:',width=8).grid(row=rows,columnspan=2,sticky=W)
-rows+=1
+        self.talktext = Text(self, width=45, height=35)
+        self.talktext.grid(row=rows, columnspan=5, padx=8)
+        sb = Scrollbar(self)
+        sb.grid(row=rows, column=4, sticky=E + W + S + N)
+        self.talktext['yscrollcommand'] = sb.set
+        sb['command'] = self.talktext.yview
+        rows += 1
 
-talktext = Text(window,width=45,height=35)
-talktext.grid(row=rows,columnspan=5,padx=8)
-sb = Scrollbar(window)
-sb.grid(row=rows,column=4,sticky=E+W+S+N)
-talktext['yscrollcommand'] = sb.set
-sb['command']=talktext.yview
-rows+=1
+        inputf = Frame(self)
+        inputf.grid(row=rows)
+        Label(inputf, text='輸入:', width=5).grid(row=rows, sticky=W)
+        self.inputentry = Entry(inputf, width=20)
+        self.inputentry.grid(row=rows, column=1, columnspan=3, pady=5)
+        self.inputentry.bind('<Return>', lambda x: self.enter())
+        rows += 1
 
-inputf=Frame(window)
-inputf.grid(row=rows)
-Label(inputf,text='輸入:',width=5).grid(row=rows,sticky=W)
-inputentry = Entry(inputf,width=20)
-inputentry.grid(row=rows,column=1,columnspan=3,pady=5)
-inputentry.bind('<Return>',lambda x:enter())
-rows+=1
-
-info = StringVar()
-info.set('歡迎使用P2P')
-message = Label(window,textvariable=info,width = 25)
-message.grid(row=rows,columnspan=5,sticky=N,pady=15)
-getprint = threading.Thread(target=waitprint,args=())
-getprint.setDaemon(True)
-getprint.start()
-getport = threading.Thread(target=port,args=())
-getport.setDaemon(True)
-getport.start()
-window.mainloop()
+        self.info = StringVar()
+        self.info.set('歡迎使用P2P')
+        message = Label(self, textvariable=self.info, width=25)
+        message.grid(row=rows, columnspan=5, sticky=N, pady=15)
+        self.mainloop()
+    def setIp(self):
+        ip = str(self.ipentry.get())
+        self._print_('準備連接:'+ip)
+        self.net = tkmain.net(ip,self)
+        '''start = threading.Thread(target = self.net.server(),args=())
+        start.setDaemon(True)
+        start.start()'''
+    def enter(self):
+        self.net.user(self.inputentry.get())
+        self.inputentry.delete(0,END)
+    def _print_(self,s):
+        print(s)
+        self.info.set(s)
+    def add_new(self,s):
+        self.talktext.insert(END, '\n' + s)
+        self.talktext.see(END)
+        print(s)
+window()

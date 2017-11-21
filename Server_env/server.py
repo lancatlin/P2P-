fd639c3495d930f63319730ca1f1ba75bd127678
Lancat <lancat1499@gmail.com>
2017-11-21 20:49:15 +0800

    加入pydrive

--- Server_env/server.py
+++ Server_env/server.py
@@ -1,1 +1,12 @@
-#test
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+gauth = GoogleAuth()
+url = gauth.GetAuthUrl()
+code = 
+
+drive = GoogleDrive(gauth)
+
+file = drive.CreateFile({'title': 'Hello.txt'})
+file.SetContentString('Hello World!')
+file.Upload()
--- Server_env/test_colient.py
+++ Server_env/test_colient.py
@@ -4,7 +4,7 @@ ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
 key = paramiko.RSAKey.from_private_key(open('id_rsa'))
-ssh.connect(hostname='192.168.1.41',username='lancatlin',pkey=key)
+ssh.connect(hostname='github.com/lancatlin/P2P-',username='lancatlin',pkey=key,timeout=10)
 ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command('uptime')
 print(ssh_stdout.read())
 ssh.close()

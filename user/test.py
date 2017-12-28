c24563ee811683634ed7e43a6ac34e3e6393ecd8
lancat <lancat1499@gmail.com>
2017-12-28 14:17:19 +0000

    加入網頁，加入測試文件，加入server資料夾

---
+++ user/test.py
@@ -0,0 +1,22 @@
+import sheet
+import NetWork
+
+
+class test:
+    def test_sheet(self,s):
+        ip_sheet = sheet.GetIP(s)
+        ip_sheet.set_IP('test', '0.0.0.0', '192.168.1.255', 12345)
+        i = ip_sheet.search('test')
+        if i is None:
+            return '無法查找到test'
+        elif i not in ip_sheet.get_all():
+            return 'get_all 不符合結果'
+        ip_sheet.clear('test')
+        i = ip_sheet.search('test')
+        if i is not None:
+            return '刪除失敗'
+        return '測試成功'
+
+
+t = test()
+print(t.test_sheet('IP'))
